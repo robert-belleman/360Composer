7d1d095170f4cc468c1c80df01ea8ab92193523e
Derkades <robin+github@rkslot.nl>
2023-04-14 13:51:14 +0200

    Tweak proxy for new docker setup

--- scene-editor/frontend/src/setupProxy.js
+++ scene-editor/frontend/src/setupProxy.js
@@ -4,25 +4,25 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'http://backend:5000',
       changeOrigin: true,
     })
   );
 
   // bit hacky but it works
-  app.use(
-    '/asset/*.mp4',
-    createProxyMiddleware({
-      target: 'http://localhost:3000/app',
-      changeOrigin: true,
-    })
-  );
+  // app.use(
+  //   '/asset/*.mp4',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:3000/app',
+  //     changeOrigin: true,
+  //   })
+  // );
 
-  app.use(
-    '/asset/*.glb',
-    createProxyMiddleware({
-      target: 'http://localhost',
-      changeOrigin: true,
-    })
-  );
+  // app.use(
+  //   '/asset/*.glb',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost',
+  //     changeOrigin: true,
+  //   })
+  // );
 };
