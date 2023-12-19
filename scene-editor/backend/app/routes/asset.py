5baa7d1de1362fef1ab5fe044227c1282cb4d564
Yoshi Fu <yoshifu2002@gmail.com>
2023-12-19 12:18:38 +0100

    reverted the removal of files on the server because scenes or scenarios might use them.

--- scene-editor/backend/app/routes/asset.py
+++ scene-editor/backend/app/routes/asset.py
@@ -77,21 +77,12 @@ class DeleteAsset(Resource):
 
     @user_jwt_required
     @project_access_required
-    def delete(self, id):
+    def post(self, id):
         """
         Deletes the assets with the given ids
         """
         asset: AssetModel
         asset = AssetModel.query.filter_by(id=id.split(".")[0]).first_or_404()
-
-        # Delete the video file, thumbnail and hls playlist from the server.
-        try:
-            Path(ASSET_DIR, asset.path).unlink()
-            Path(ASSET_DIR, asset.thumbnail_path).unlink()
-            if asset.hls_path:
-                Path(ASSET_DIR, asset.hls_path).unlink()
-        except FileNotFoundError:
-            pass
 
         db.session.delete(asset)
         db.session.commit()
