321f87baffbdbe4d8f0f9fddedb060e3956e0cf2
Sander de Beet <sander447debeet@gmail.com>
2024-07-31 19:37:59 +0200

    use own docker image packages

--- docker-push.sh
+++ docker-push.sh
@@ -4,5 +4,8 @@ set -ex
 # Script to push production images to the GitHub container registry.
 # To be used by members of the Visualisation Lab.
 
-docker buildx build -t 'ghcr.io/visualisationlab/360composer-backend'  -f scene-editor/backend/ops/Dockerfile  --push scene-editor/backend
-docker buildx build -t 'ghcr.io/visualisationlab/360composer-frontend' -f scene-editor/frontend/ops/Dockerfile --push scene-editor/frontend
+# docker buildx build -t 'ghcr.io/visualisationlab/360composer-backend'  -f scene-editor/backend/ops/Dockerfile  --push scene-editor/backend
+# docker buildx build -t 'ghcr.io/visualisationlab/360composer-frontend' -f scene-editor/frontend/ops/Dockerfile --push scene-editor/frontend
+
+docker buildx build -t 'ghcr.io/SlashiZard/360composer-backend'  -f scene-editor/backend/ops/Dockerfile  --push scene-editor/backend
+docker buildx build -t 'ghcr.io/SlashiZard/360composer-frontend' -f scene-editor/frontend/ops/Dockerfile --push scene-editor/frontend
