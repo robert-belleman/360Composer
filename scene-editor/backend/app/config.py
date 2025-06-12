8dcf32b4e4a709398c09bb30a4cf200b0857d8a1
KimberleyR1 <kim.roks2001@gmail.com>
2025-06-13 00:25:34 +0200

    config.py updated

--- scene-editor/backend/app/config.py
+++ scene-editor/backend/app/config.py
@@ -4,6 +4,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI")
 RESTPLUS_VALIDATE = True
 ASSET_DIR = environ.get("ASSET_DIR")
+AUDIOASSET_DIR = environ.get("AUDIOASSET_DIR")
 REDIS_URL = environ.get("REDIS_URL", "redis://redis:6379/0")
 QUEUES = environ.get("QUEUES", "default").split(",")
 JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY")
